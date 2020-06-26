dd05a3ec4a71465011adaf9fa26336a459f6694e
Akn <sibata486@gmail.com>
2020-06-26 22:23:42 +0900

    機能追加 / 起動してからの時間が毎秒更新されるよう変更

--- NewShutDownTimer/MainWindowViewModel.cs
+++ NewShutDownTimer/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using Prism.Mvvm;
 
 namespace NewShutDownTimer {
@@ -21,6 +22,16 @@ namespace NewShutDownTimer {
             get {
                 return timeForShutdown.ToString(@"MM/dd HH\:mm\:ss");
             }
+        }
+
+        public MainWindowViewModel() {
+
+            Timer timer = new Timer(1000);
+            timer.Elapsed += (object sender, ElapsedEventArgs e) => {
+                RaisePropertyChanged(nameof(ElapsedTimeFromStart)); 
+            };
+
+            timer.Start();
         }
     }
 }
