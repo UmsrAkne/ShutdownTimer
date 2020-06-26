56506996428a5bbd3ab7ef588e7b2613adf43646
Akn <sibata486@gmail.com>
2020-06-26 21:54:31 +0900

    機能追加 / アプリの起動からの経過時間、シャットダウンまでの時間のテキストをバインド

--- NewShutDownTimer/MainWindowViewModel.cs
+++ NewShutDownTimer/MainWindowViewModel.cs
@@ -7,5 +7,20 @@ using Prism.Mvvm;
 
 namespace NewShutDownTimer {
     class MainWindowViewModel : BindableBase{
+
+        private DateTime startUpDate = DateTime.Now;
+        public String ElapsedTimeFromStart {
+            get {
+                TimeSpan span = DateTime.Now - startUpDate;
+                return span.ToString(@"d\.hh\:mm\:ss");
+            }
+        }
+
+        private DateTime timeForShutdown = DateTime.Now.AddHours(3);
+        public String TimeForShutdown {
+            get {
+                return timeForShutdown.ToString(@"MM/dd HH\:mm\:ss");
+            }
+        }
     }
 }
