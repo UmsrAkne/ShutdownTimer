c868eb59f927868471c1c6be9612fc8c4ea00d2a
Akn <sibata486@gmail.com>
2021-11-25 22:52:44 +0900

    refactor / コードを整形

--- NewShutDownTimer/MainWindowViewModel.cs
+++ NewShutDownTimer/MainWindowViewModel.cs
@@ -1,69 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Timers;
-using System.Windows;
-using Prism.Commands;
-using Prism.Mvvm;
-
-namespace NewShutDownTimer
+﻿namespace NewShutDownTimer
 {
-    class MainWindowViewModel : BindableBase
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using System.Timers;
+    using System.Windows;
+    using Prism.Commands;
+    using Prism.Mvvm;
+
+    public class MainWindowViewModel : BindableBase
     {
-
-        public Window Window { private get; set; }
-
         private DateTime startUpDate = DateTime.Now;
-        public String ElapsedTimeFromStart
-        {
-            get
-            {
-                TimeSpan span = DateTime.Now - startUpDate;
-                return span.ToString(@"d\.hh\:mm\:ss");
-            }
-        }
-
         private DateTime timeForShutdown = DateTime.Now.AddHours(3);
-        public String TimeForShutdown
-        {
-            get
-            {
-                return timeForShutdown.ToString(@"MM/dd HH\:mm\:ss");
-            }
-        }
-
-        public String RemainingTimeUntilShutDown
-        {
-            get
-            {
-                return (timeForShutdown - DateTime.Now).ToString(@"hh\:mm\:ss") + " " + getRemainingTimeMeter();
-            }
-        }
-
-        /// <summary>
-        /// 残り時間を大まかに示すメーター（文字列）を取得します。
-        /// </summary>
-        public String getRemainingTimeMeter()
-        {
-            const int meterUnit = 15;
-            var remainingTime = timeForShutdown - DateTime.Now;
-            int meterLength = (int)remainingTime.TotalMinutes / meterUnit;
-
-            char meterCharacter = '/';
-            return new string(meterCharacter, meterLength);
-        }
-
-        private Boolean preShutdownNotified = false;
-
-        public DelegateCommand<object> ChangeRemainingTimeCommand { get; private set; }
+        private bool preShutdownNotified = false;
 
         public MainWindowViewModel()
         {
-
             Timer timer = new Timer(1000);
             timer.Elapsed += (object sender, ElapsedEventArgs e) =>
             {
@@ -79,14 +35,13 @@ namespace NewShutDownTimer
                         {
                             Window.Activate();
                             MessageBox.Show(Window, "シャットダウンまで残り15分です");
-                        }
-                        );
+                        });
                     }
                 }
 
                 if (timeForShutdown.CompareTo(DateTime.Now) < 0)
                 {
-                    shutdown();
+                    Shutdown();
                     timer.Stop();
                 }
             };
@@ -96,7 +51,7 @@ namespace NewShutDownTimer
             ChangeRemainingTimeCommand = new DelegateCommand<object>(
                 (object param) =>
                 {
-                    String buttonTag = (String)param;
+                    string buttonTag = (string)param;
                     int additionMinutes = int.Parse(buttonTag);
                     timeForShutdown = timeForShutdown.AddMinutes(additionMinutes);
                     RaisePropertyChanged(nameof(TimeForShutdown));
@@ -106,21 +61,61 @@ namespace NewShutDownTimer
                         preShutdownNotified = false;
                     }
                 },
-                (object param) => { return true; }
-            );
-
+                (object param) => { return true; });
         }
 
-        private void shutdown()
+        public Window Window { private get; set; }
+
+        public string ElapsedTimeFromStart
         {
-            // shutdown.exe を実行するコード
+            get
+            {
+                TimeSpan span = DateTime.Now - startUpDate;
+                return span.ToString(@"d\.hh\:mm\:ss");
+            }
+        }
+
+        public string TimeForShutdown
+        {
+            get
+            {
+                return timeForShutdown.ToString(@"MM/dd HH\:mm\:ss");
+            }
+        }
+
+        public string RemainingTimeUntilShutDown
+        {
+            get
+            {
+                return (timeForShutdown - DateTime.Now).ToString(@"hh\:mm\:ss") + " " + GetRemainingTimeMeter();
+            }
+        }
+
+        public DelegateCommand<object> ChangeRemainingTimeCommand { get; private set; }
+
+        /// <summary>
+        /// 残り時間を大まかに示すメーター（文字列）を取得します。
+        /// </summary>
+        public string GetRemainingTimeMeter()
+        {
+            const int MeterUnit = 15;
+            var remainingTime = timeForShutdown - DateTime.Now;
+            int meterLength = (int)remainingTime.TotalMinutes / MeterUnit;
+
+            char meterCharacter = '/';
+            return new string(meterCharacter, meterLength);
+        }
+
+        private void Shutdown()
+        {
+            /// shutdown.exe を実行するコード
             ProcessStartInfo psi = new ProcessStartInfo();
             psi.FileName = "shutdown.exe";
 
-            //コマンドラインを指定
+            ///コマンドラインを指定
             psi.Arguments = "/s";
 
-            //ウィンドウを表示しないようにする
+            ///ウィンドウを表示しないようにする
             psi.UseShellExecute = false;
             psi.CreateNoWindow = true;
 
